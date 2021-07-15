a6be4c27f3e70b475e3d99424ff7848e1f4e772f
Pranav Dhopey <pranav.dhope@gmail.com>
2021-07-15 18:25:24 +0530

    output modified

--- google-cloud-storage/my_bucket/outputs.tf
+++ google-cloud-storage/my_bucket/outputs.tf
@@ -15,58 +15,19 @@
  */
 
 
-output "bucket" {
+output "my_bucket" {
   description = "Bucket resource."
   value       = module.my_bucket.bucket
 }
 
 
-/*
-output "name" {
+output "my_bucket_name" {
   description = "Bucket name"
-  value       = module.gcs_bucket.name
+  value       = module.my_bucket.bucket.name
 }
 
 
-
-output "url" {
+output "my_bucket_url" {
   description = "Bucket URL."
-  value       = module.gcs_bucket.url
+  value       = module.my_bucket.bucket.url
 }
-
-
-output "buckets" {
-  description = "Bucket resources as list."
-  value       = local.buckets_list
-}
-
-output "buckets_map" {
-  description = "Bucket resources by name."
-  value       = google_storage_bucket.buckets
-}
-
-
-output "names" {
-  description = "Bucket names."
-  value = { for name, bucket in google_storage_bucket.buckets :
-    name => bucket.name
-  }
-}
-
-output "urls" {
-  description = "Bucket URLs."
-  value = { for name, bucket in google_storage_bucket.buckets :
-    name => bucket.url
-  }
-}
-
-output "names_list" {
-  description = "List of bucket names."
-  value       = local.buckets_list[*].name
-}
-
-output "urls_list" {
-  description = "List of bucket URLs."
-  value       = local.buckets_list[*].url
-}
-*/
